4fa71f869df2935d0f2036a6783583981fbe63b8
MightyBitey <Thetoughpup@gmail.com>
2026-03-10 04:36:23 -0600

    Added Debug Macros.

--- Source/GameDevCourse/GameDevCourse.cpp
+++ Source/GameDevCourse/GameDevCourse.cpp
@@ -3,5 +3,6 @@
 #include "GameDevCourse.h"
 #include "Modules/ModuleManager.h"
 
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameDevCourse, "GameDevCourse" );
  
--- Source/GameDevCourse/Private/Item/Item.cpp
+++ Source/GameDevCourse/Private/Item/Item.cpp
@@ -1,35 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "GameDevCourse/DebugMacros.h"
 #include "Item/Item.h"
 
-// Sets default values
+#include "GameDevCourse/GameDevCourse.h"
+/////////////////////////////////////////////CONSTRUCTORS////////////////////////////////////////////////////////////
 AItem::AItem()
 {
-
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
-
+////////////////////////////////////////FUNCTIONS////////////////////////////////////////////////////////////////////
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Beginplay Called!"));
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan,FString("Item on screen!"));
-	}
-
-	
 }
 
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	//Movement Rate in Units of cm/s
+	float MovementRate = 50.f;
+	float RotationRate = 45.f;
+	
+	// MovementRate * DeltaTime (cm/s) * (s/frame) = (cm/frame)
+	AddActorWorldOffset(FVector((MovementRate * DeltaTime), 0.f, 0.f));
+	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+	DRAW_SPHERE_SingleFrame(GetActorLocation());
+	DRAW_VECTOR_SingleFrame(GetActorLocation(),GetActorLocation() + (GetActorForwardVector() * 100.f));
 }
 
