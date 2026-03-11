a1c969b945a51476621623a70ac2d5a580e2f775
MightyBitey <Thetoughpup@gmail.com>
2026-03-10 21:01:49 -0600

    Template Functions and Sin+Cos Functions

--- Source/GameDevCourse/Private/Item/Item.cpp
+++ Source/GameDevCourse/Private/Item/Item.cpp
@@ -23,25 +23,27 @@ void AItem::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 	RunningTime += DeltaTime;
 
-	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
-
-	AddActorWorldOffset(FVector(0.f,0.f, DeltaZ));
-
-	
-	//FString PrintTime = FString::Printf(TEXT("Current Time: %f"), Time);
-	//GEngine -> AddOnScreenDebugMessage(-1,-1.f, FColor::Cyan, *PrintTime);
-
-
-
-
+	//float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+	//AddActorWorldOffset(FVector(0.f,0.f, DeltaZ));
 	//Movement Rate in Units of cm/s
 	float MovementRate = 50.f;
 	float RotationRate = 45.f;
 	
-	// MovementRate * DeltaTime (cm/s) * (s/frame) = (cm/frame)
-	//AddActorWorldOffset(FVector((MovementRate * DeltaTime), 0.f, 0.f));
-	//AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_VECTOR_SingleFrame(GetActorLocation(),GetActorLocation() + (GetActorForwardVector() * 100.f));
+
+	FVector AvgVector = Avg<FVector>(GetActorLocation(),FVector::ZeroVector);
+	DRAW_POINT_SingleFrame(AvgVector);
 }
+
+float AItem::TransformedSin()
+{
+	return  Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return  Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
 
--- Source/GameDevCourse/Public/Item/Item.h
+++ Source/GameDevCourse/Public/Item/Item.h
@@ -12,15 +12,35 @@ class GAMEDEVCOURSE_API AItem : public AActor
 	GENERATED_BODY()
 	
 public:
-	
-	
 	AItem();
 virtual void Tick(float DeltaTime) override;
+
 protected:
-private:
-	float RunningTime = 0.f;
-	float Amplitude = .25f;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Sine Parameters")
 	float TimeConstant = 5.f;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Sine Parameters")
+	float Amplitude = .25f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+
+	template<typename T>
+	T Avg(T First, T Second);
+
+private:
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RunningTime = 0.f;
 	
 	virtual void BeginPlay() override;
 };
+
+template <typename T>
+T AItem::Avg(T First, T Second)
+{
+	return (First + Second) / 2; 
+}
